19baa20f271830a98bd8df52ba072222adb58763
ninaalerasoul <ninaalerasoul@gmail.com>
2022-02-10 12:49:01 +0330

    init + first config

---
+++ app/src/main/java/com/example/onlineshopapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.onlineshopapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import com.example.onlineshopapp.ui.theme.OnlineShopAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            OnlineShopAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+
+                }
+            }
+        }
+    }
+}
